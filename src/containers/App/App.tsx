76f34471c03c3da8bbf8812bc6f0e3f7d446854e
Yury Baykov <y.baykov@corp.mail.ru>
2019-03-02 20:47:12 +0300

    counter component

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -9,18 +9,9 @@
  */
 
 import React from 'react';
-import { Platform, Text, View } from 'react-native';
-import styles from './App.styles';
 import { Provider } from 'react-redux';
 import configureStore from '../../modules/configureStore';
-
-const instructions = Platform.select({
-	ios: `Press Cmd+R to reload,\n
-		Cmd+D or shake for dev menu`,
-	android:
-		`Double tap R on your keyboard to reload,\n
-		Shake or press menu button for dev menu`
-});
+import Counter from '../Counter';
 
 interface Props {
 }
@@ -29,11 +20,7 @@ export default class App extends React.Component<Props> {
 	public render(): React.ReactNode {
 		return (
 			<Provider store={configureStore()}>
-				<View style={styles.container}>
-					<Text style={styles.welcome}>Welcome to React Native!</Text>
-					<Text style={styles.instructions}>To get started, edit App.tsx</Text>
-					<Text style={styles.instructions}>{instructions}</Text>
-				</View>
+				<Counter />
 			</Provider>
 		);
 	}
---
+++ src/containers/Counter/Counter.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { counterDecAction, counterIncAction, CounterState, resetAction } from '../../modules/counter';
+import { StoreState } from '../../modules/index/';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+
+interface OwnProps {
+
+}
+
+interface StoreProps extends CounterState{
+
+}
+
+interface DispatchProps {
+	incCounter: () => void;
+	decCounter: () => void;
+	resetCounter: () => void;
+}
+
+type Props = OwnProps & StoreProps & DispatchProps;
+
+class Counter extends React.Component<Props> {
+	public render(): React.ReactNode {
+		return undefined;
+	}
+}
+
+function mapStateToProps(state: StoreState): StoreProps {
+	const { counter } = state;
+
+	return counter;
+}
+
+function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
+	return {
+		incCounter: () => dispatch(counterIncAction()),
+		decCounter: () => dispatch(counterDecAction()),
+		resetCounter: () => dispatch(resetAction())
+	};
+}
+
+export default connect<StoreProps, DispatchProps, any, any>(mapStateToProps, mapDispatchToProps)(Counter);
