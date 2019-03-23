c0f780b1f01c9b44f04e3396ef2e0545317b2bfd
juliacherry12 <vishne-yulya@yandex.ru>
2019-03-23 18:25:05 +0300

    Базовый класс AuthForm.

---
+++ src/containers/AuthForm/AuthForm.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Button, Text, View, TextInput } from 'react-native';
+
+export default class AuthForm extends React.Component<void> {
+	public render(): React.ReactNode {
+		return (
+			<View>
+				<TextInput />
+				<TextInput />
+				<Button title={'Вход'} onPress={() => {}}/>
+			</View>
+		);
+	}
+}
