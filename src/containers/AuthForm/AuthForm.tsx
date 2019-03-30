c17850ddff4fa3169afe7464bdac86757df8c2f8
juliacherry12 <vishne-yulya@yandex.ru>
2019-03-30 18:31:31 +0300

    Сверстали форму авторизации

--- src/containers/AuthForm/AuthForm.tsx
+++ src/containers/AuthForm/AuthForm.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
 import { Button, Text, View, TextInput } from 'react-native';
 import styles from '../Counter/Counter.styles';
+import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
 
 export default class AuthForm extends React.Component<{}> {
 	public render(): React.ReactNode {
 		return (
-			<View style={styles.container}>
-				<TextInput placeholder={'Логин'} />
-				<TextInput placeholder={'Пароль'} />
-				<Button title={'Вход'} onPress={() => {}}/>
-			</View>
+			<Container>
+				<Header />
+				<Content>
+					<Form>
+						<Item floatingLabel>
+							<Label>Username</Label>
+							<Input />
+						</Item>
+						<Item floatingLabel last>
+							<Label>Password</Label>
+							<Input />
+						</Item>
+					</Form>
+				</Content>
+			</Container>
 		);
 	}
 }
