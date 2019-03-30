674f7b01ddb26bc5e4ab41c4a7ca2168dcf21a54
juliacherry12 <vishne-yulya@yandex.ru>
2019-03-30 17:33:15 +0300

    Стиль для AuthForm

---
+++ src/containers/AuthForm/AuthForm.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'column', // flex-direction: column
+		justifyContent: 'flex-start',
+		alignItems: 'center'
+	}
+});
+
+export default styles;
--- src/containers/AuthForm/AuthForm.tsx
+++ src/containers/AuthForm/AuthForm.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { Button, Text, View, TextInput } from 'react-native';
+import styles from '../Counter/Counter.styles';
 
 export default class AuthForm extends React.Component<{}> {
 	public render(): React.ReactNode {
 		return (
-			<View>
-				<TextInput />
-				<TextInput />
+			<View style={styles.container}>
+				<TextInput placeholder={'Логин'} />
+				<TextInput placeholder={'Пароль'} />
 				<Button title={'Вход'} onPress={() => {}}/>
 			</View>
 		);
