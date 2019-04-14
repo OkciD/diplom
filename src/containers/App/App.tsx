14d2d3f70f9ab39686dd66b101ef7a8b191f087a
Yury Baykov <y.baykov@corp.mail.ru>
2019-04-15 01:37:43 +0300

    successful connection & reading from db

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -12,7 +12,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../../modules/configureStore';
 import Counter from '../Counter';
-import SQLite from 'react-native-sqlite-storage';
+import SQLite, { SQLError } from 'react-native-sqlite-storage';
+import { Alert } from 'react-native';
 
 interface Props {
 }
@@ -24,5 +25,35 @@ export default class App extends React.Component<Props> {
 				<Counter />
 			</Provider>
 		);
+	}
+
+	public componentDidMount(): void {
+		SQLite.DEBUG(true);
+		// SQLite.enablePromise(true);
+
+		// @ts-ignore
+		const db: SQLite.SQLiteDatabase = SQLite.openDatabase({
+			name: 'counter-db.sqlite3',
+			createFromLocation: 1,
+			readOnly: true
+		}, () => {
+			Alert.alert('DB successfully opened');
+		}, (e: SQLError) => {
+			Alert.alert(`Error while opening DB: ${e.message}`);
+		});
+
+		db.transaction((transaction: SQLite.Transaction) => {
+			transaction.executeSql(
+				'SELECT * FROM counts',
+				[],
+				(transaction: SQLite.Transaction, resultSet: SQLite.ResultSet) => {
+					const temp: string[] = [];
+					for (let i = 0; i < resultSet.rows.length; i += 1) {
+						temp.push(JSON.stringify(resultSet.rows.item(i)));
+					}
+					Alert.alert(`Successfull read: ${temp.join(', ')}`);
+				}
+			);
+		});
 	}
 }
