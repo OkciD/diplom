67b9acf13661576ea94e548b6923f50272bc7eda
juliacherry12 <vishne-yulya@yandex.ru>
2019-04-15 21:43:08 +0300

    связали компоненты СТАТЕ

--- src/containers/AuthForm/AuthForm.tsx
+++ src/containers/AuthForm/AuthForm.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react';
 import { Container, Header, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
 import styles from './AuthForm.styles';
+import AuthState from '../../modules/auth/auth.types';
+import { StoreState } from '../../modules/index';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { setFieldAction } from '../../modules/auth';
+import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native';
 
-export default class AuthForm extends React.Component<{}> {
+interface OwnProps {
+
+}
+
+interface StoreProps extends AuthState {
+
+}
+
+interface DispatchProps {
+	setLogin: (login: string) => void;
+	setPassword: (password: string) => void;
+}
+
+type Props = OwnProps & StoreProps & DispatchProps;
+
+class AuthForm extends React.Component<Props> {
 	public render(): React.ReactNode {
 		return (
 			<Container>
@@ -11,11 +32,14 @@ export default class AuthForm extends React.Component<{}> {
 					<Form>
 						<Item floatingLabel>
 							<Label>Username</Label>
-							<Input />
+							<Input value={this.props.login} onChange={(event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+								// @ts-ignore
+								console.warn(event.target.text);
+							}} />
 						</Item>
 						<Item floatingLabel>
 							<Label>Password</Label>
-							<Input secureTextEntry/>
+							<Input secureTextEntry value={this.props.password}/>
 						</Item>
 					</Form>
 					<Button hasText block style={styles.button}>
@@ -26,3 +50,16 @@ export default class AuthForm extends React.Component<{}> {
 		);
 	}
 }
+
+function mapStateToProps(state: StoreState): StoreProps {
+	return state.auth;
+}
+
+function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
+	return {
+		setLogin: login => dispatch(setFieldAction({ login })),
+		setPassword: password => dispatch(setFieldAction({ password }))
+	};
+}
+
+export default connect<StoreProps, DispatchProps, any, any>(mapStateToProps, mapDispatchToProps)(AuthForm);
