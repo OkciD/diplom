3a4c3f3fb72400a40743002889ccd04b1808a966
Yury Baykov <y.baykov@corp.mail.ru>
2019-04-16 04:45:06 +0300

    initial db module

---
+++ src/modules/db/db.actions.ts
@@ -0,0 +1,20 @@
+import SQLite from 'react-native-sqlite-storage';
+import { DB_NAME } from './';
+
+let db: SQLite.SQLiteDatabase;
+
+export async function openDb(): Promise<void> {
+	SQLite.DEBUG(true); // TODO: remove for production
+	SQLite.enablePromise(true);
+
+	// @ts-ignore
+	db = await SQLite.openDatabase({
+		name: DB_NAME,
+		createFromLocation: 1,
+		readOnly: true
+	});
+}
+
+export async function closeDb(): Promise<void> {
+	await db.close();
+}
---
+++ src/modules/db/index.ts
@@ -0,0 +1,2 @@
+export { DB_NAME } from './db.constants';
+export { openDb, closeDb } from './db.actions';
