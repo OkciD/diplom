3a6fa0d1925369abd17a397c535d56b393068867
Yury Baykov <y.baykov@corp.mail.ru>
2019-04-16 04:45:34 +0300

    not rendering app until db is opened

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -12,48 +12,43 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../../modules/configureStore';
 import Counter from '../Counter';
-import SQLite, { SQLError } from 'react-native-sqlite-storage';
-import { Alert } from 'react-native';
+import { closeDb, openDb } from '../../modules/db';
 
 interface Props {
+
 }
 
-export default class App extends React.Component<Props> {
+interface State {
+	ready: boolean;
+}
+
+export default class App extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+
+		this.state = { ready: false };
+		console.log('App created');
+	}
+
+	public componentWillMount(): void {
+		openDb()
+			.then<void>(() => {
+				console.log('App is ready');
+				this.setState({
+					ready: true
+				});
+			});
+	}
+
+	public componentWillUnmount(): void {
+		closeDb();
+	}
+
 	public render(): React.ReactNode {
-		return (
+		return (!this.state.ready) ? null : (
 			<Provider store={configureStore()}>
 				<Counter />
 			</Provider>
 		);
-	}
-
-	public componentDidMount(): void {
-		SQLite.DEBUG(true);
-		// SQLite.enablePromise(true);
-
-		// @ts-ignore
-		const db: SQLite.SQLiteDatabase = SQLite.openDatabase({
-			name: 'counter-db.sqlite3',
-			createFromLocation: 1,
-			readOnly: true
-		}, () => {
-			Alert.alert('DB successfully opened');
-		}, (e: SQLError) => {
-			Alert.alert(`Error while opening DB: ${e.message}`);
-		});
-
-		db.transaction((transaction: SQLite.Transaction) => {
-			transaction.executeSql(
-				'SELECT * FROM counts',
-				[],
-				(transaction: SQLite.Transaction, resultSet: SQLite.ResultSet) => {
-					const temp: string[] = [];
-					for (let i = 0; i < resultSet.rows.length; i += 1) {
-						temp.push(JSON.stringify(resultSet.rows.item(i)));
-					}
-					Alert.alert(`Successfull read: ${temp.join(', ')}`);
-				}
-			);
-		});
 	}
 }
