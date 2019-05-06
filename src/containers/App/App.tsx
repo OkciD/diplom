eaf1a13e7d51090879ff9f3c035bef191ac76e1c
Yury Baykov <y.baykov@corp.mail.ru>
2019-05-06 23:42:45 +0300

    oops

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -59,7 +59,7 @@ export default class App extends React.Component<Props, State> {
 	public render(): React.ReactNode {
 		return (!this.state.ready) ? null : ( // TODO: null -> preloader
 			<Provider store={configureStore()}>
-				<Counter />
+				<BodyPartSelector />
 			</Provider>
 		);
 	}
