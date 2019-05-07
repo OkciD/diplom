305f2c5414ff285a469756428cf0550f0bcf7691
Yury Baykov <y.baykov@corp.mail.ru>
2019-05-08 01:38:48 +0300

    reduced body model

--- src/components/HumanBody/HumanBody.tsx
+++ src/components/HumanBody/HumanBody.tsx
@@ -19,8 +19,8 @@ interface Paths {
 // tslint:disable-next-line:function-name
 export default function HumanBody(props: Props): React.ReactElement {
 	const {
-		width = 1.7 * 168,
-		height = 1.7 * 320,
+		width = 1.65 * 168,
+		height = 1.65 * 320,
 		onBodyPartPress = () => {},
 		selectedBodyPartId = null,
 		gender = 'male',
