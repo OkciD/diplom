ba98fc405bda3f9eb617afa9be7d42f07511477a
Yury Baykov <y.baykov@corp.mail.ru>
2019-05-10 21:45:27 +0300

    saving router stack to store

--- src/containers/Router/Router.tsx
+++ src/containers/Router/Router.tsx
@@ -2,29 +2,42 @@ import React from 'react';
 import {
 	default as ReactNativeEasyRouter,
 	RouterProps as ReactNativeEasyRouterProps,
-	Router as ReactNativeEasyRouterObject
+	Router as ReactNativeEasyRouterObject,
+	RouterStack
 } from 'react-native-easy-router';
-import { ConnectedComponentClass } from 'react-redux';
+import { connect, ConnectedComponentClass } from 'react-redux';
 import { RouterProps } from './';
+import { Dispatch } from 'redux';
+import { setStackAction } from '../../modules/routing';
 
 interface RoutingElementParams {
 	router: ReactNativeEasyRouterObject;
 	[paramName: string]: any;
 }
 
-interface Props {
+interface OwnProps {
 	routes: {
 		[routeName: string]: ConnectedComponentClass<any, RouterProps>;
 	};
 	initialRoute: string;
 }
 
+interface StoreProps {
+
+}
+
+interface DispatchProps {
+	setStack: (stack: RouterStack) => void;
+}
+
+type Props = OwnProps & StoreProps & DispatchProps;
+
 /**
  * @class Router
  * @description Обёртка над react-native-easy-router, привязающая его к Redux
  * @see https://github.com/sergeyshpadyrev/react-native-easy-router
  */
-export default class Router extends React.Component<Props> {
+class Router extends React.Component<Props> {
 	private createRoutes(): ReactNativeEasyRouterProps['routes'] {
 		return Object.entries(this.props.routes).reduce(
 			(routes: ReactNativeEasyRouterProps['routes'], [routeName, Component]) => ({
@@ -38,11 +51,26 @@ export default class Router extends React.Component<Props> {
 	}
 
 	public render(): React.ReactNode {
+		const { initialRoute, setStack } = this.props;
+
 		return (
 			<ReactNativeEasyRouter
 				routes={this.createRoutes()}
-				initialRoute={this.props.initialRoute}
+				initialRoute={initialRoute}
+				onStackChange={setStack}
 			/>
 		);
 	}
 }
+
+function mapStateToProps(): StoreProps {
+	return {};
+}
+
+function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
+	return {
+		setStack: (stack: RouterStack) => dispatch(setStackAction({ stack }))
+	};
+}
+
+export default connect<StoreProps, DispatchProps, any, any>(mapStateToProps, mapDispatchToProps)(Router);
