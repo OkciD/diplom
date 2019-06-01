bf7e11d5c109a748127a32ac8e352023f1afc3f6
Yury Baykov <y.baykov@corp.mail.ru>
2019-06-01 19:54:27 +0300

    first try

--- src/containers/SymptomSearch/SymptomSearch.styles.ts
+++ src/containers/SymptomSearch/SymptomSearch.styles.ts
@@ -1,7 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+	container: {
+		marginRight: 10,
+		marginLeft: 10,
+		height: '100%'
+	},
+	searchBarContainer: {
+		marginTop: 2,
+		paddingTop: 0
+	}
 });
 
 export default styles;
--- src/containers/SymptomSearch/SymptomSearch.tsx
+++ src/containers/SymptomSearch/SymptomSearch.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import { StoreState } from '../../modules/index';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
+import { RouterProps } from '../Router';
+// @ts-ignore
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
+import { Badge, Icon, Input, Item, View, Text } from 'native-base';
+import styles from './SymptomSearch.styles';
 
 interface OwnProps {
 
@@ -15,11 +20,58 @@ interface DispatchProps {
 
 }
 
-type Props = OwnProps & StoreProps & DispatchProps;
+type Props = OwnProps & StoreProps & DispatchProps & RouterProps;
 
 class SymptomSearch extends React.Component<Props> {
+	private readonly gestureRecognizerConfig = {
+		velocityThreshold: 0.3,
+		directionalOffsetThreshold: 80
+	};
+
+	private readonly suggestions: string[] = [];
+	private readonly selectedSymptoms: string[] = ['Озноб', 'Недосып', 'Потеря в весе', 'Боль в руке'];
+
+	private readonly onSwipe = (gestureName: string) => {
+		const { SWIPE_LEFT } = swipeDirections;
+		const { router } = this.props;
+
+		switch (gestureName) {
+			case SWIPE_LEFT: {
+				router.push.BodyPartSelector({}, { type: 'right' });
+				return;
+			}
+			default: {
+				return;
+			}
+		}
+	};
+
 	public render(): React.ReactNode {
-		return null;
+		return (
+			<GestureRecognizer config={this.gestureRecognizerConfig} onSwipe={this.onSwipe}>
+				<View style={styles.container}>
+					<Item style={styles.searchBarContainer}>
+						<Input placeholder="Поиск" />
+						<Icon
+							type="MaterialCommunityIcons"
+							name="magnify"
+						/>
+					</Item>
+					{this.selectedSymptoms.map((symptom) => (
+						<Badge primary key={symptom}>
+							<Text>
+								{symptom}
+								<Icon
+									style={{ fontSize: 18, color: '#FFF', lineHeight: 25 }}
+									type="MaterialCommunityIcons"
+									name="close-circle"
+								/>
+							</Text>
+						</Badge>
+					))}
+				</View>
+			</GestureRecognizer>
+		);
 	}
 }
 
