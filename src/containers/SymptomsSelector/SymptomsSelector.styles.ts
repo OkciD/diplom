8015a8ec8d7ff156a53048f48a67cfb61e86d3ee
Yury Baykov <y.baykov@corp.mail.ru>
2019-07-31 22:15:20 +0300

    removed unused styles

--- src/containers/SymptomsSelector/SymptomsSelector.styles.ts
+++ src/containers/SymptomsSelector/SymptomsSelector.styles.ts
@@ -12,34 +12,6 @@ const styles = StyleSheet.create({
 		marginTop: 2,
 		paddingTop: 0
 	},
-	selectedSymptomsContainer: {
-		marginTop: 20,
-
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'flex-start',
-		alignItems: 'flex-start',
-		flexWrap: 'wrap',
-		alignContent: 'space-around'
-	},
-	selectedSymptomBadge: {
-		marginBottom: 10,
-		marginRight: 10,
-
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'flex-start',
-		alignItems: 'center'
-	},
-	selectedSymptomBadgeText: {
-		fontSize: 20,
-		lineHeight: 22
-	},
-	selectedSymptomBadgeDeleteIcon: {
-		marginLeft: 3,
-		fontSize: 20,
-		color: '#FFFFFF'
-	},
 	fab: {
 		backgroundColor: commonColor.brandPrimary
 	}
